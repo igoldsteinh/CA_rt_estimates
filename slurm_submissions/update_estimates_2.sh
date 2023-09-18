d47e6bdfe8e163994eff606f70c776fd4636329a
igoldsteinh <61714888+igoldsteinh@users.noreply.github.com>
2023-09-18 15:17:24 -0700

    going up to four hour time limit, only running trinity

--- slurm_submissions/update_estimates_2.sh
+++ slurm_submissions/update_estimates_2.sh
@@ -4,12 +4,12 @@
 #SBATCH -A vminin_lab ## account to charge
 #SBATCH -N 1          ## run on a single node
 #SBATCH -n 4          ## request 4 tasks (4 CPUs)
-#SBATCH -t 03:00:00   ## 1 hr run time limit
+#SBATCH -t 04:00:00   ## 4 hr run time limit
 #SBATCH --mem=4G 
 #SBATCH -o update_estimates_2-%A-%a.out
 #SBATCH --mail-type=begin,end
 #SBATCH --mail-user=igoldst1@uci.edu
-#SBATCH --array=0-56
+#SBATCH --array=51
 module purge
 module load R
 cd //pub/igoldst1/CA_rt_estimates
