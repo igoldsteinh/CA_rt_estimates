0e171779bcdd8c82cc0184a1f7e93d2434c5331e
Isaac Goldstein <igoldst1@login-i17.local>
2023-09-29 16:41:17 -0700

    update 9/29/23

--- slurm_submissions/update_estimates_2.sh
+++ slurm_submissions/update_estimates_2.sh
@@ -4,12 +4,12 @@
 #SBATCH -A vminin_lab ## account to charge
 #SBATCH -N 1          ## run on a single node
 #SBATCH -n 4          ## request 4 tasks (4 CPUs)
-#SBATCH -t 04:00:00   ## 4 hr run time limit
+#SBATCH -t 06:00:00   ## 4 hr run time limit
 #SBATCH --mem=4G 
 #SBATCH -o update_estimates_2-%A-%a.out
 #SBATCH --mail-type=begin,end
 #SBATCH --mail-user=igoldst1@uci.edu
-#SBATCH --array=51
+#SBATCH --array=36-39,40-41
 module purge
 module load R
 cd //pub/igoldst1/CA_rt_estimates
